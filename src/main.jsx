fdd6015f71435eb4800f9c92a5e258363f3e9674
Vetradu10P <vetradu10pgmail.com>
2024-04-29 14:11:30 +0200

    En cours

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-)
+  </HashRouter>
+);
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    base: '/',
+    base: './',
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },

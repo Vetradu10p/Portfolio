8ea14d8981003bb684506eedfd85880b9c479d5a
Vetradu10P <vetradu10pgmail.com>
2024-04-29 14:28:25 +0200

    Lien html ajouté

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,15 +3,18 @@ import React from "react";
 import '../../Assets/Css/card.css'
 
 const Card = ({ project }) => {
-    const { cover, description, title, link } = project;
+    const { cover, description, title } = project;
+    const link = 'https://vetradu10p.github.io/Openclassrooms-Booki/';
 
     return (
         <li className='Card'>
-            <div className='ImageContainer'>
-                <img className='Card-image' src={cover} alt="Card d'un projet" />
-            </div>
-            <p className='Description'>{description}</p>
-            <h2 className="Title">{title}</h2>
+            <a href={link} target="_blank" rel="noopener noreferrer" className="Card-link">
+                <div className='ImageContainer'>
+                    <img className='Card-image' src={cover} alt="Card d'un projet" />
+                </div>
+                <p className='Description'>{description}</p>
+                <h2 className="Title">{title}</h2>
+            </a>
         </li>
     );
 };
